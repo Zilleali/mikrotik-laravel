5f42a9c71413afcac94f056b360272f8b45636c5
ZILLEALI <63631432+Zilleali@users.noreply.github.com>
2026-05-15 21:29:06 +0500

    fix: all tests passing locally

--- config/mikrotik.php
+++ config/mikrotik.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'host'     => env('MIKROTIK_HOST', '192.168.88.1'),
+    'host'     => env('MIKROTIK_HOST', '192.168.10.1'),
     'port'     => env('MIKROTIK_PORT', 8728),
-    'username' => env('MIKROTIK_USER', 'admin'),
-    'password' => env('MIKROTIK_PASS', ''),
+    'username' => env('MIKROTIK_USER', 'test'),
+    'password' => env('MIKROTIK_PASS', 'test'),
     'timeout'  => env('MIKROTIK_TIMEOUT', 10),
 ];
