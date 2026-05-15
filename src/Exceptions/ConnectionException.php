67ffb0f9c981b705f14086409094a1dceb327688
ZILLEALI <63631432+Zilleali@users.noreply.github.com>
2026-05-15 23:16:00 +0500

    feat(core): RouterosClient with full comments and unit tests

---
+++ src/Exceptions/ConnectionException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ZillEAli\MikrotikLaravel\Exceptions;
+
+use RuntimeException;
+
+/**
+ * ConnectionException
+ *
+ * Thrown when a TCP connection to the MikroTik router
+ * cannot be established, or the connection is lost
+ * during communication.
+ *
+ * @package ZillEAli\MikrotikLaravel\Exceptions
+ * @author  Zill E Ali <zilleali1245@gmail.com>
+ * @link    https://zilleali.com
+ */
+class ConnectionException extends RuntimeException {}
