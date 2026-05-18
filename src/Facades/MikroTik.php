b398a9e187c6c1252aa2900884195b51df4d7489
ZILLEALI <63631432+Zilleali@users.noreply.github.com>
2026-05-18 21:41:41 +0500

    feat(dns): add DnsManager with static entries, cache flush, server config

--- src/Facades/MikroTik.php
+++ src/Facades/MikroTik.php
@@ -35,6 +35,12 @@ use ZillEAli\MikrotikLaravel\Services\SystemManager;
  * @method static BridgeManager bridge() // Bridge Manager for managing Mikrotik bridges
  * @method static IpAddressManager ipAddress() // IP Address Manager for managing IP addresses on Mikrotik devices
  * @method static ArpManager arp() // ARP Manager for managing ARP entries on Mikrotik devices
+ *  @method static DnsManager dns() // DNS Manager for managing DNS settings and static entries on Mikrotik devices
+ *
+ *
+ *
+ *
+ *
  *
  * @see MikrotikManager
  *
